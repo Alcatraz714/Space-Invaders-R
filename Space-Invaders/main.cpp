9b096bbaab273403773ebc492aa7d97b6def8962
Rishi Saxena <saxenarishi34@gmail.com>
2024-07-29 15:11:02 +0530

    OOP

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,4 +1,37 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+
+class Player
+{
+private:
+
+    // Private Properties
+    int health = 3;
+    sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+    int movement_speed = 5;
+    int player_score = 0;
+
+public:
+
+    // Public Properties
+    sf::Texture player_texture;
+    sf::Sprite player_sprite;
+
+    //Public Getter & Setter methods
+    int getScore() {
+        return player_score;
+    };
+
+    void setScore(int newScore) {
+        player_score = newScore;
+    };
+
+    //New methods
+    void takeDamage() {};
+    void move() {};
+    void shootBullets() {};
+};
 
 int main()
 {
@@ -20,47 +53,7 @@ int main()
         // Clear the window---
         window.clear(sf::Color::Black);
 
-        //Start
-
-        // Draw a circle
-        sf::CircleShape circle(50); // Radius 50
-        circle.setFillColor(sf::Color::Green);
-        circle.setPosition(100, 100); // Set position
-        window.draw(circle);
-
-        //Draw a Red Square
-        sf::RectangleShape square(sf::Vector2f(50, 50));
-        square.setFillColor(sf::Color::Red);
-        square.setPosition(200, 200); // Set position
-        window.draw(square);
-
-        //Draw a Blue Triangle
-        sf::CircleShape Triangle(50, 3);
-        Triangle.setFillColor(sf::Color::Blue);
-        Triangle.setPosition(300, 300); // Set position
-        window.draw(Triangle);
-
-        //Drawing a sprite 
-        sf::Texture outscal_texture;
-        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
-
-        sf::Sprite outscal_sprite;
-        outscal_sprite.setTexture(outscal_texture);
-
-        outscal_sprite.setPosition(500, 200); // Position
-        outscal_sprite.setRotation(45); // Rotation in degrees
-        outscal_sprite.setScale(0.5, 0.5); // Scale factor
-
-        window.draw(outscal_sprite);
-
-        //Drawing a text
-        sf::Font font;
-        font.loadFromFile("assets/fonts/OpenSans.ttf");
-        sf::Text text("SFML is Awesome", font, 50);
-        text.setFillColor(sf::Color::White);
-        window.draw(text);
-
-        //End
+        Player player;
 
         // Display whatever you draw
         window.display();
