56f32a4b0a3727e61060e90b78abc775c4b7fecf
Rishi Saxena <saxenarishi34@gmail.com>
2024-08-08 13:08:16 +0530

    Added Bullet folder and interface

---
+++ Space-Invaders/Header/Bullet/BulletService.h
@@ -0,0 +1,17 @@
+#pragma once
+namespace Bullet
+{
+	class BulletService
+	{
+	private:
+		
+
+	public:
+		BulletService();
+		~BulletService();
+
+		void initialize();
+		void update();
+		void render();
+	};
+}
---
+++ Space-Invaders/Source/Bullet/BulletService.cpp
@@ -0,0 +1,24 @@
+#include "../../Header/Bullet/BulletService.h"
+
+namespace Bullet
+{
+	Bullet::BulletService::BulletService()
+	{
+	}
+
+	Bullet::BulletService::~BulletService()
+	{
+	}
+
+	void Bullet::BulletService::initialize()
+	{
+	}
+
+	void Bullet::BulletService::update()
+	{
+	}
+
+	void Bullet::BulletService::render()
+	{
+	}
+}
